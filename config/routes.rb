9d3139f12a793399bf11b2cb78bdcbf6149c09f0
Your Name <dkgs1998@gmail.com>
2016-10-13 02:35:18 +0300

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "alert_ranges#index"
   resources :alert_ranges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
